724c24cc1f23ff508ab311d3821dc5db7eb13ada
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-05-21 17:19:37 +0200

    updated to 1.20.6

---
+++ src/main/java/hibi/blahaj/BlahajDataComponentTypes.java
@@ -0,0 +1,22 @@
+package hibi.blahaj;
+
+import net.minecraft.component.*;
+import net.minecraft.registry.*;
+import net.minecraft.text.*;
+import net.minecraft.util.*;
+
+import java.util.function.*;
+
+public class BlahajDataComponentTypes {
+
+	public static final DataComponentType<Text> OWNER = register("owner", (builder) -> builder.codec(TextCodecs.STRINGIFIED_CODEC).packetCodec(TextCodecs.REGISTRY_PACKET_CODEC).cache());
+
+	private static <T> DataComponentType<T> register(String id, UnaryOperator<DataComponentType.Builder<T>> builderOperator) {
+		return Registry.register(Registries.DATA_COMPONENT_TYPE, new Identifier(Blahaj.MOD_ID, id), builderOperator.apply(DataComponentType.builder()).build());
+	}
+
+	public static void register() {
+
+	}
+
+}
